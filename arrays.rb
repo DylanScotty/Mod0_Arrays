49990648e053aa01cb929d7c74314c29935bd699
Dylan Timmons <dylanstimmons@icloud.com>
2023-04-12 16:08:31 -0600

    add methods to arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ friends = ["Joe", "Toni", "Andy", "Ben"]
 age = [23, 25, 27, 29]
 cash = [27.00, 100.00, 21.00, 34.00]
 wants_to_go_out = [true, false, true, false]
+
+# pop method of friends array will remove "Ben" from the array
+friends.pop
+
+# push method on age array will add an item to the end of the array
+age.shift(45)
+
+# shift method of cash array will remove 27.00 from the array
+cash.shift
+
+# unshift method on wants_to_go_out will add a new item to the beginning of the array
+wants_to_go_out(false)
