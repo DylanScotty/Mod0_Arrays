60a28dd0945c38e596e1dfa0da0e66999061e9c9
Dylan Timmons <dylanstimmons@icloud.com>
2023-04-12 16:14:13 -0600

    Change variable for Booleans

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,7 @@
 friends = ["Joe", "Toni", "Andy", "Ben"]
 age = [23, 25, 27, 29]
 cash = [27.00, 100.00, 21.00, 34.00]
-wants_to_go_out = [true, false, true, false]
+is_happy = [true, false, true, false]
 
 # pop method of friends array will remove "Ben" from the array
 friends.pop
@@ -12,5 +12,12 @@ age.shift(45)
 # shift method of cash array will remove 27.00 from the array
 cash.shift
 
-# unshift method on wants_to_go_out will add a new item to the beginning of the array
-wants_to_go_out(false)
+# unshift method on is_happy will add a new item to the beginning of the array
+is_happy(false)
+
+#Index positions 
+#index positions start with 0 and move up by 1. When calling your first position you will always start with 0
+#how to call the first friend 
+friends(0)
+#how to call the 3rd position in age
+age(2)
